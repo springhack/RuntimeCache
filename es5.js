47d8a771b1480070783688e769446f30cbb074e8
chenfuchun <chenfuchun@bytedance.com>
2017-04-25 13:49:41 +0800

    Link list
    Change-Id: I722ce744b3962d253fcbb3ce159480a313b83db0

--- es5.js
+++ es5.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -10,8 +10,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 /**
         Author: SpringHack - springhack@live.cn
-        Last modified: 2017-04-24 23:39:55
-        Filename: src/utils/runtimeCache.js
+        Last modified: 2017-04-25 13:34:12
+        Filename: es6.js
         Description: Created by SpringHack using vim automatically.
 **/
 
@@ -19,9 +19,89 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
  * 除非明确调用clean方法否则timeBaseQueue中的缓存不会清除，建议直接setAutoClean(true)
  */
 
+var Node = function () {
+  function Node(value) {
+    _classCallCheck(this, Node);
+
+    this.value = null;
+    this.next = null;
+
+    this.value = value;
+  }
+
+  _createClass(Node, [{
+    key: 'setNext',
+    value: function setNext(next) {
+      this.next = next;
+    }
+  }, {
+    key: 'getNext',
+    value: function getNext() {
+      return this.next;
+    }
+  }, {
+    key: 'setValue',
+    value: function setValue(value) {
+      this.value = value;
+    }
+  }, {
+    key: 'getValue',
+    value: function getValue() {
+      return this.value;
+    }
+  }]);
+
+  return Node;
+}();
+
+var List = function () {
+  function List() {
+    _classCallCheck(this, List);
+
+    this.head = null;
+    this.tail = null;
+
+    this.head = null;
+    this.tail = null;
+  }
+
+  _createClass(List, [{
+    key: 'getHead',
+    value: function getHead() {
+      return this.head;
+    }
+  }, {
+    key: 'getTail',
+    value: function getTail() {
+      return this.tail;
+    }
+  }, {
+    key: 'moveAfter',
+    value: function moveAfter() {
+      this.head = this.head.getNext();
+      if (this.head === null) {
+        this.tail = null;
+      }
+    }
+  }, {
+    key: 'pushBack',
+    value: function pushBack(value) {
+      if (!this.head && !this.tail) {
+        this.head = new Node(value);
+        this.tail = this.head;
+      } else {
+        this.tail.setNext(new Node(value));
+        this.tail = this.tail.getNext();
+      }
+    }
+  }]);
+
+  return List;
+}();
+
 var Cache = function () {
   _createClass(Cache, null, [{
-    key: "getInstance",
+    key: 'getInstance',
     value: function getInstance() {
       if (!Cache.instance) {
         for (var _len = arguments.length, rest = Array(_len), _key = 0; _key < _len; _key++) {
@@ -33,7 +113,7 @@ var Cache = function () {
       return Cache.instance;
     }
   }, {
-    key: "setInstance",
+    key: 'setInstance',
     value: function setInstance(instance) {
       Cache.instance = instance;
     }
@@ -43,49 +123,44 @@ var Cache = function () {
     var timeout = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 20;
     var autoCleanInterval = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
     var autoClean = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+    var debug = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : false;
 
     _classCallCheck(this, Cache);
 
     this.timer = null;
+    this.debug = false;
     this.timeout = 0;
     this.interval = 0;
     this.instance = null;
     this.autoClean = false;
     this.checkQueue = {};
-    this.timeBaseQueue = [];
+    this.timeBaseList = new List();
 
     this.timeout = 20;
+    this.debug = debug;
     this.interval = autoCleanInterval;
-    /* eslint-disable */
-    console.info(new Date().toString() + " \u7F13\u5B58\u5F00\u542F\uFF0C\u8D85\u65F6\u8BBE\u7F6E: " + this.timeout + "\uFF0C\u68C0\u6D4B\u95F4\u9694: " + this.interval);
-    /* eslint-enable */
+    this.info('\u7F13\u5B58\u5F00\u542F\uFF0C\u8D85\u65F6\u8BBE\u7F6E: ' + this.timeout + '\uFF0C\u68C0\u6D4B\u95F4\u9694: ' + this.interval);
     this.setAutoClean(autoClean);
   }
 
   _createClass(Cache, [{
-    key: "fetch",
+    key: 'fetch',
     value: function fetch(key) {
       if (this.checkQueue[key]) {
         var time = new Date().getTime();
         if (time - this.checkQueue[key].time > this.timeout * 1000) {
           delete this.checkQueue[key];
-          /* eslint-disable */
-          console.info(new Date().toString() + " " + key + " \u7F13\u5B58\u5DF2\u7ECF\u8FC7\u671F\uFF0C\u6267\u884C\u79FB\u9664");
-          /* eslint-enable */
+          this.info(key + ' \u7F13\u5B58\u5DF2\u7ECF\u8FC7\u671F\uFF0C\u6267\u884C\u79FB\u9664');
           return null;
         }
-        /* eslint-disable */
-        console.info(new Date().toString() + " " + key + " \u7F13\u5B58\u672A\u8FC7\u671F\uFF0C\u53D6\u5F97\u503C");
-        /* eslint-enable */
+        this.info(key + ' \u7F13\u5B58\u672A\u8FC7\u671F\uFF0C\u53D6\u5F97\u503C');
         return this.checkQueue[key].value;
       }
-      /* eslint-disable */
-      console.info(new Date().toString() + " \u5565\uFF1F\u662F\u4E0D\u662F\u65E9\u5C31\u8FC7\u671F\u4E86\uFF1F\u8FD8\u662F\u538B\u6839\u5C31\u6CA1\u5B58\uFF1F");
-      /* eslint-enable */
+      this.info('啥？是不是早就过期了？还是压根就没存？');
       return null;
     }
   }, {
-    key: "append",
+    key: 'append',
     value: function append(key) {
       var value = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
 
@@ -93,66 +168,62 @@ var Cache = function () {
       if (this.checkQueue[key]) {
         this.checkQueue[key].time = time;
         this.checkQueue[key].value = value;
-        /* eslint-disable */
-        console.info(new Date().toString() + " \u66F4\u65B0 " + key + " \u7F13\u5B58");
-        /* eslint-enable */
+        this.info('\u66F4\u65B0 ' + key + ' \u7F13\u5B58');
       } else {
         this.checkQueue[key] = {
           key: key,
           time: time,
           value: value
         };
-        this.timeBaseQueue.push(this.checkQueue[key]);
-        /* eslint-disable */
-        console.info(new Date().toString() + " \u65B0\u5EFA " + key + " \u7F13\u5B58");
-        /* eslint-enable */
+        this.timeBaseList.pushBack(this.checkQueue[key]);
+        this.info('\u65B0\u5EFA ' + key + ' \u7F13\u5B58');
       }
     }
   }, {
-    key: "remove",
+    key: 'remove',
     value: function remove(key) {
       delete this.checkQueue[key];
+      this.info('\u79FB\u9664 ' + key + ' \u7F13\u5B58');
+    }
+  }, {
+    key: 'clean',
+    value: function clean() {
+      var time = new Date().getTime();
+      while (this.timeBaseList.getHead() && time - this.timeBaseList.getHead().getValue().time > this.timeout * 1000) {
+        var entry = this.timeBaseList.getHead().getValue();
+        this.info(entry.key + ' \u7F13\u5B58\u8FC7\u671F\uFF0C\u6267\u884C\u79FB\u9664');
+        this.timeBaseList.moveAfter();
+        delete this.checkQueue[entry.key];
+      }
+    }
+  }, {
+    key: 'info',
+    value: function info(_info) {
       /* eslint-disable */
-      console.info(new Date().toString() + " \u79FB\u9664 " + key + " \u7F13\u5B58");
+      this.debug && console.log(new Date().toString() + ' ' + _info);
       /* eslint-enable */
     }
   }, {
-    key: "clean",
-    value: function clean() {
-      var _this = this;
-
-      var time = new Date().getTime();
-      this.timeBaseQueue = this.timeBaseQueue.filter(function (entry) {
-        if (time - entry.time > _this.timeout * 1000) {
-          /* eslint-disable */
-          console.info(new Date().toString() + " " + entry.key + " \u7F13\u5B58\u8FC7\u671F\uFF0C\u6267\u884C\u79FB\u9664");
-          /* eslint-enable */
-          delete _this.checkQueue[entry.key];
-          return false;
-        }
-        return true;
-      });
+    key: 'setDebug',
+    value: function setDebug(debug) {
+      this.debug = debug;
     }
   }, {
-    key: "setAutoClean",
+    key: 'setAutoClean',
     value: function setAutoClean() {
-      var _this2 = this;
+      var _this = this;
 
       var auto = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
 
       if (auto !== this.autoClean) {
         if (auto && !this.timer) {
           this.timer = setInterval(function () {
-            return _this2.clean();
+            return _this.clean();
           }, this.interval * 1000);
-          /* eslint-disable */
-          console.info(new Date().toString() + " \u5F00\u542F\u81EA\u52A8\u79FB\u9664");
-          /* eslint-enable */
+          this.info('开启自动移除');
         }
         if (!auto && this.timer) {
-          /* eslint-disable */
-          console.info(new Date().toString() + " \u5173\u95ED\u81EA\u52A8\u79FB\u9664");
-          /* eslint-enable */
+          this.info('关闭自动移除');
           clearInterval(this.timer);
           this.timer = null;
         }
--- es6.js
+++ es6.js
@@ -1,7 +1,7 @@
 /**
         Author: SpringHack - springhack@live.cn
-        Last modified: 2017-04-24 23:39:55
-        Filename: src/utils/runtimeCache.js
+        Last modified: 2017-04-25 13:34:12
+        Filename: es6.js
         Description: Created by SpringHack using vim automatically.
 **/
 
@@ -9,15 +9,66 @@
  * 除非明确调用clean方法否则timeBaseQueue中的缓存不会清除，建议直接setAutoClean(true)
  */
 
+class Node {
+  value = null;
+  next = null;
+  constructor(value) {
+    this.value = value;
+  }
+  setNext(next) {
+    this.next = next;
+  }
+  getNext() {
+    return this.next;
+  }
+  setValue(value) {
+    this.value = value;
+  }
+  getValue() {
+    return this.value;
+  }
+}
+
+class List {
+  head = null;
+  tail = null;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  getHead() {
+    return this.head;
+  }
+  getTail() {
+    return this.tail;
+  }
+  moveAfter() {
+    this.head = this.head.getNext();
+    if (this.head === null) {
+      this.tail = null;
+    }
+  }
+  pushBack(value) {
+    if (!this.head && !this.tail) {
+      this.head = new Node(value);
+      this.tail = this.head;
+    } else {
+      this.tail.setNext(new Node(value));
+      this.tail = this.tail.getNext();
+    }
+  }
+}
+
 class Cache {
   static instance = null;
   timer = null;
+  debug = false;
   timeout = 0;
   interval = 0;
   instance = null;
   autoClean = false;
   checkQueue = {};
-  timeBaseQueue = [];
+  timeBaseList = new List();
   static getInstance(...rest) {
     if (!Cache.instance) {
       Cache.instance = new Cache(...rest);
@@ -27,12 +78,11 @@ class Cache {
   static setInstance(instance) {
     Cache.instance = instance;
   }
-  constructor(timeout = 20, autoCleanInterval = 1, autoClean = false) {
+  constructor(timeout = 20, autoCleanInterval = 1, autoClean = false, debug = false) {
     this.timeout = 20;
+    this.debug = debug;
     this.interval = autoCleanInterval;
-    /* eslint-disable */
-    console.info(`${(new Date()).toString()} 缓存开启，超时设置: ${this.timeout}，检测间隔: ${this.interval}`);
-    /* eslint-enable */
+    this.info(`缓存开启，超时设置: ${this.timeout}，检测间隔: ${this.interval}`);
     this.setAutoClean(autoClean);
   }
   fetch(key) {
@@ -40,19 +90,13 @@ class Cache {
       const time = (new Date()).getTime();
       if (time - this.checkQueue[key].time > this.timeout * 1000) {
         delete this.checkQueue[key];
-        /* eslint-disable */
-        console.info(`${(new Date()).toString()} ${key} 缓存已经过期，执行移除`);
-        /* eslint-enable */
+        this.info(`${key} 缓存已经过期，执行移除`);
         return null;
       }
-      /* eslint-disable */
-      console.info(`${(new Date()).toString()} ${key} 缓存未过期，取得值`);
-      /* eslint-enable */
+      this.info(`${key} 缓存未过期，取得值`);
       return this.checkQueue[key].value;
     }
-    /* eslint-disable */
-    console.info(`${(new Date()).toString()} 啥？是不是早就过期了？还是压根就没存？`);
-    /* eslint-enable */
+    this.info('啥？是不是早就过期了？还是压根就没存？');
     return null;
   }
   append(key, value = true) {
@@ -60,52 +104,47 @@ class Cache {
     if (this.checkQueue[key]) {
       this.checkQueue[key].time = time;
       this.checkQueue[key].value = value;
-      /* eslint-disable */
-      console.info(`${(new Date()).toString()} 更新 ${key} 缓存`);
-      /* eslint-enable */
+      this.info(`更新 ${key} 缓存`);
     } else {
       this.checkQueue[key] = {
         key,
         time,
         value
       };
-      this.timeBaseQueue.push(this.checkQueue[key]);
-      /* eslint-disable */
-      console.info(`${(new Date()).toString()} 新建 ${key} 缓存`);
-      /* eslint-enable */
+      this.timeBaseList.pushBack(this.checkQueue[key]);
+      this.info(`新建 ${key} 缓存`);
     }
   }
   remove(key) {
     delete this.checkQueue[key];
-    /* eslint-disable */
-    console.info(`${(new Date()).toString()} 移除 ${key} 缓存`);
-    /* eslint-enable */
+    this.info(`移除 ${key} 缓存`);
   }
   clean() {
     const time = (new Date()).getTime();
-    this.timeBaseQueue = this.timeBaseQueue.filter((entry) => {
-      if (time - entry.time > this.timeout * 1000) {
-        /* eslint-disable */
-        console.info(`${(new Date()).toString()} ${entry.key} 缓存过期，执行移除`);
-        /* eslint-enable */
-        delete this.checkQueue[entry.key];
-        return false;
-      }
-      return true;
-    });
+    while (this.timeBaseList.getHead()
+      && time - this.timeBaseList.getHead().getValue().time > this.timeout * 1000) {
+      const entry = this.timeBaseList.getHead().getValue();
+      this.info(`${entry.key} 缓存过期，执行移除`);
+      this.timeBaseList.moveAfter();
+      delete this.checkQueue[entry.key];
+    }
+  }
+  info(info) {
+    /* eslint-disable */
+    this.debug && console.log(`${(new Date()).toString()} ${info}`);
+    /* eslint-enable */
+  }
+  setDebug(debug) {
+    this.debug = debug;
   }
   setAutoClean(auto = true) {
     if (auto !== this.autoClean) {
       if (auto && !this.timer) {
         this.timer = setInterval(() => this.clean(), this.interval * 1000);
-        /* eslint-disable */
-        console.info(`${(new Date()).toString()} 开启自动移除`);
-        /* eslint-enable */
+        this.info('开启自动移除');
       }
       if (!auto && this.timer) {
-        /* eslint-disable */
-        console.info(`${(new Date()).toString()} 关闭自动移除`);
-        /* eslint-enable */
+        this.info('关闭自动移除');
         clearInterval(this.timer);
         this.timer = null;
       }
